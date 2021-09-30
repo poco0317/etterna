35ff8497d4887cf9cb50cb80f2d69ab3386fc57d
Barinade <poco0317@users.noreply.github.com>
2021-09-29 19:31:59 -0500

    fix unix compile 2

--- src/Etterna/Screen/Others/ScreenInstallOverlay.cpp
+++ src/Etterna/Screen/Others/ScreenInstallOverlay.cpp
@@ -222,7 +222,7 @@ DoInstalls(CommandLineActions::CommandLineArgs args)
 					std::ifstream INFILE(path,
 										 std::ios::in | std::ifstream::binary);
 					INFILE.seekg(0, std::ios::end);
-					newVector.resize(unsigned int(INFILE.tellg() / sizeof(NoteInfo)));
+					newVector.resize(static_cast<unsigned>(INFILE.tellg() / sizeof(NoteInfo)));
 					INFILE.seekg(0, std::ios::beg);
 					INFILE.read((char*)&newVector[0],
 								newVector.capacity() * sizeof(NoteData));
